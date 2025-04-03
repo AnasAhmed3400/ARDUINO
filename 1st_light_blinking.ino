85c8bf928ac791eba7e55b3c7294a5f84a10491c
AnasAhmed3400 <154613047+AnasAhmed3400@users.noreply.github.com>
2025-04-03 17:47:32 +0500

    Add files via upload

---
+++ 1st_light_blinking.ino
@@ -0,0 +1,21 @@
+void setup() {
+  pinMode(10,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(10,HIGH);
+  delay(1000);
+  digitalWrite(10,LOW);
+  delay(300);
+  digitalWrite(8,HIGH);
+  delay(1000);
+  digitalWrite(8,LOW);
+  delay(200);
+  digitalWrite(6,HIGH);
+  delay(1000);
+  digitalWrite(6,LOW);
+  delay(100);
+
+}
